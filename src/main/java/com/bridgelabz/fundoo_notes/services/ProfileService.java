192550d38eb979ef1a0b362092c5ed9fce85a976
Rakshak D S <wasdlifeqecrlove@gmail.com>
2020-01-31 11:43:57 +0530

    [rak]final commit

--- src/main/java/com/bridgelabz/fundoo_notes/services/ProfileService.java
+++ src/main/java/com/bridgelabz/fundoo_notes/services/ProfileService.java
@@ -1,16 +1,16 @@
 package com.bridgelabz.fundoo_notes.services;
 
-import org.springframework.web.multipart.MultipartFile;
-
-import com.amazonaws.services.s3.model.S3Object;
-import com.bridgelabz.fundoo_notes.Entity.Profile;
-
-public interface ProfileService {
-
-	Profile storeObjectInS3(MultipartFile file, String originalFilename, String contentType, String token);
-
-	Profile updateObejctInS3(MultipartFile file, String originalFilename, String contentType, String token);
-
-	S3Object getProfilePic(String token);
-   
-}
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import com.amazonaws.services.s3.model.S3Object;
+//import com.bridgelabz.fundoo_notes.Entity.Profile;
+//
+//public interface ProfileService {
+//
+//	Profile storeObjectInS3(MultipartFile file, String originalFilename, String contentType, String token);
+//
+//	Profile updateObejctInS3(MultipartFile file, String originalFilename, String contentType, String token);
+//
+//	S3Object getProfilePic(String token);
+//   
+//}
